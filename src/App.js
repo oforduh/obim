086b8fb69df9fa22e4dfced9605cff368893e7b3
oforduhh <oforduhh@gmail.com>
2021-09-01 09:16:46 +0100

    fkffkfk

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styles from "./styles/app.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Coming soon</h1>
+    </div>
+  );
+}
+
+export default App;
